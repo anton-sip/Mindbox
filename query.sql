4eab576757d55f1ae9d62d7efee48064a91f3f79
Антон Сипко <sipkoantonz@gmail.com>
2024-03-17 21:00:22 +0700

    добавлен sql запрос

---
+++ query.sql
@@ -0,0 +1,47 @@
+﻿CREATE TABLE Products (
+                          Id INT PRIMARY KEY,
+                          "Name" TEXT
+);
+
+INSERT INTO Products
+VALUES
+    (1, 'Chicken'),
+    (2, 'Eggs'),
+    (3, 'Prague cake'),
+    (4, 'Bread'),
+    (5, 'Juice');
+
+CREATE TABLE Categories (
+                            Id INT PRIMARY KEY,
+                            "Name" TEXT
+);
+
+INSERT INTO Categories
+VALUES
+    (1, 'Meat'),
+    (2, 'Animal products'),
+    (3, 'Flour'),
+    (4, 'Dessert');
+
+CREATE TABLE ProductCategories (
+                                   ProductId INT FOREIGN KEY REFERENCES Products(Id),
+                                   CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
+                                   PRIMARY KEY (ProductId, CategoryId)
+);
+
+INSERT INTO ProductCategories
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 2),
+    (3, 3),
+    (3, 4),
+    (4, 3);
+
+
+SELECT Products.Name, Categories.Name 
+FROM Products
+         LEFT JOIN ProductCategories
+                   ON Products.Id = ProductCategories.ProductId
+         LEFT JOIN Categories
+                   ON ProductCategories.CategoryId = Categories.Id
